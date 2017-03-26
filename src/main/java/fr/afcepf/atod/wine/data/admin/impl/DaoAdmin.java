1b9286a10d2f11c68d8edafb93895bab40678e20
ronan <ronan.lachater@gmail.com>
2017-03-26 01:08:41 +0100

    daoAdmin update

--- src/main/java/fr/afcepf/atod/wine/data/admin/impl/DaoAdmin.java
+++ src/main/java/fr/afcepf/atod/wine/data/admin/impl/DaoAdmin.java
@@ -1,18 +1,16 @@
 package fr.afcepf.atod.wine.data.admin.impl;
 
-import java.util.List;
-
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
-import fr.afcepf.atod.vin.data.exception.WineException;
 import fr.afcepf.atod.wine.data.admin.api.IDaoAdmin;
 import fr.afcepf.atod.wine.data.impl.DaoGeneric;
 import fr.afcepf.atod.wine.entity.Admin;
 
 @Service
 @Transactional
-public class DaoAdmin extends DaoGeneric<Admin, Integer> implements IDaoAdmin {
+public class DaoAdmin extends DaoGeneric<Admin, Integer> 
+	implements IDaoAdmin {
 
 }
